cfa8d8b6d69082e4bccb6a51ac4071b0e3d4be00
Harshit <harshitkyal@gmail.com>
2019-02-12 14:58:55 +0530

    changed port setting

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var connection = sql.createConnection({
   database: 'testing'
 });
 ///middleware to process your request
-app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.urlencoded({ extended: false }))
 //app.use(morgan('combined'))
 
 connection.connect(function (error) {
@@ -26,34 +26,34 @@ connection.connect(function (error) {
   }
 
 });
- 
-app.post("/user_create",(request,response)=>{
+
+app.post("/user_create", (request, response) => {
   console.log("trying to create  new user")
 
   console.log("firNAme" + request.body.create_first_name)
   const firstName = request.body.create_first_name
   const lastName = request.body.create_last_name
-  
+
   const queryString = "insert into new_table (a,idd) values(?,?)"
-  connection.query(queryString,[firstName,lastName],(error,results,fields)=>{
-    if(error){
-      console.log("Failed to add new entry" +error)
+  connection.query(queryString, [firstName, lastName], (error, results, fields) => {
+    if (error) {
+      console.log("Failed to add new entry" + error)
       response.sendStatus(500)
       return
     }
-    else{
+    else {
       console.log("Inserted successfully with Id")
       response.end()
     }
   })
   response.end()
 })
-app.get('/user/:id',function(request,response)  {
+app.get('/user/:id', function (request, response) {
   console.log("fecthing " + request.params.id)
   //const connection = mysql.cre
   const userId = request.params.id
   const queryString = "select * from new_table where idd = ?"
-  connection.query(queryString,[userId],(error,rows,fields)=>{
+  connection.query(queryString, [userId], (error, rows, fields) => {
     if (error) {
       console.log("Failed to query f" + error)
       response.sendStatus(500)
@@ -63,35 +63,36 @@ app.get('/user/:id',function(request,response)  {
     // let user1 = {firstName:"Harshit",lastName:"Kyal"}
     console.log(rows)
     response.json(rows)
-  }) 
+  })
   //response.end();
 })
 
-app.delete('/user/:id',(request,response)=>{
+app.delete('/user/:id', (request, response) => {
   const userId = request.params.id
   const queryString = "delete  from new_table where id = ?"
-  connection.query(queryString,[userId],(error,request,response) => {
-    if(!error)
+  connection.query(queryString, [userId], (error, request, response) => {
+    if (!error)
       response.send("Delete Successfully")
     else
       console.log(error)
   })
 })
-app.set('port', (process.env.PORT || 5000))
+PORT = process.env.PORT || 5000;
+// app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/users', function (request, response) {
   ///response.send('Hello World!')
   connection.query('SELECT * FROM new_table', function (error, rows, fields) {
-    if(!!error) {
-      console.log('Error in query',error);
-    } else{
+    if (!!error) {
+      console.log('Error in query', error);
+    } else {
       response.json(rows)
       console.log('successfull query');
     }
   });
 })
 
-app.listen(app.get('port'), function () {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(PORT, function () {
+  console.log("Node app is running at localhost:" + PORT)
 })
