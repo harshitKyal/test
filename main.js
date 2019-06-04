8ccafd1fd28f1f705d343f705a5a377d331656dd
harshitkyal <harshitkyal@gmail.com>
2019-06-05 00:32:58 +0530

    f

--- main.js
+++ main.js
@@ -4848,8 +4848,8 @@ var ApiService = /** @class */ (function () {
     };
     ApiService.prototype.apiCaller = function (type, url, data) {
         //  this.uri = this.api + url;
-        // this.uri = 'http://localhost:8100' + url;
-        this.uri = 'http://api.testautotech.xyz' + url;
+        this.uri = 'http://localhost:8100' + url;
+        //  this.uri = 'http://api.testautotech.xyz' + url;
         if (type === 'get') {
             return this.get(this.uri);
         }
